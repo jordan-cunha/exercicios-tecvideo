4ef5843bb3dd49635ba5a459a619ad619ca3a3b7
jordancunha94 <jordancunha@icloud.com>
2025-11-10 21:03:05 +0100

    02-valorjantar

---
+++ 02-valorjantar.js
@@ -0,0 +1,13 @@
+// Valor jantar: R$80,00
+// Taxa garçom: R$8,00
+// Valor a pagar: R$88,00
+
+let valorjantar = 80;
+let taxagarcom = 10;
+let total = 0;
+
+total = 80 + ((taxagarcom / 100) * 80);
+
+console.log ("Valor do jantar: R$" + valorjantar + ",00")
+console.log ("Taxa garçom: R$" + (taxagarcom / 100) * 80 + ",00")
+console.log ("Valor a pagar: R$" + total + ",00")
