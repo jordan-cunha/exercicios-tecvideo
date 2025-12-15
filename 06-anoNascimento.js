0fceb1e1650582525bc0e5af75a0ad585e9ed3e9
jordancunha94 <jordancunha@icloud.com>
2025-12-15 22:44:52 +0100

    Exercicios6 e 7

---
+++ 06-anoNascimento.js
@@ -0,0 +1,11 @@
+let anoAtual = 2025
+let anoNascimento = 1994
+let idade = 0
+
+idade = (anoAtual - anoNascimento);
+
+console.log ("Estamos no ano de " + anoAtual);
+console.log ("Eu nasci no ano de " + anoNascimento);
+console.log ("Eu tenho " + idade + " anos");
+
+
